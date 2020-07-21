80a295c19d9ad235361fa2e0eee76db603d5b3b8
GreenRamSilgen <ps3iswow@gmail.com>
2020-07-20 18:03:31 -0700

    dragula successful list movement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import dragula from 'dragula'
 
+
 let drake = dragula();
-drake.containers.push(document.querySelector("#left"));
-drake.containers.push(document.querySelector("#right"));
+drake.containers.push(document.querySelector("#container1"));
+drake.containers.push(document.querySelector("#container2"));
+drake.containers.push(document.querySelector("#container3"));
+
+
+// drake.containers.push(document.querySelector("#left"));
+// drake.containers.push(document.querySelector("#center"));
+// drake.containers.push(document.querySelector("#right"));
