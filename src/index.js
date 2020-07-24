ad399c4017f39bd97ab7a64ae3205f9087b2bb66
GreenRamSilgen <ps3iswow@gmail.com>
2020-07-23 17:23:59 -0700

    remove biglist from UI added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {
     listController
 } from './listlogic'
 
+//TODO: MAKE ADD ITEM BUTTON FUNCTIONAL FOR EACH LIST
+//TODO: UI AND LOGIC NEED TO BE UPDATED AT SAME TIME
 let drake = dragula();
 document.querySelectorAll(".b-card__body").forEach((block) => {
     drake.containers.push(block);
@@ -19,14 +21,16 @@ document.querySelectorAll(".b-card__body").forEach((block) => {
 
 let UIController = (function () {
     let DOMStrings = {
+        
         newList: 'newList',
-        newListModal : '.bg-modal',
+        newListModal: '.bg-modal',
         newListSubmit: 'modalSubmit',
         closePop: '.close',
 
         board: 'board',
 
         //CardDom
+        dataGroup: 'data-group',
         cardHolder: 'b-card',
         cardHead: 'b-card__head',
         cardHeadTitle: 'b-card__head--title',
@@ -44,15 +48,24 @@ let UIController = (function () {
     let closeBtn = document.querySelector(DOMStrings.closePop);
     let newListSubmit = document.getElementById(DOMStrings.newListSubmit);
     let popped = false;
+    //!Remove big list
+    let rmvListBtn = document.querySelectorAll("." + DOMStrings.cardHeadClose);
 
-    function makeDiv(className){
+    function findBigListX() {
+        rmvListBtn = document.querySelectorAll("." + DOMStrings.cardHeadClose);
+    }
+
+    function makeDiv(className, id) {
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', className);
+        newDiv.setAttribute(DOMStrings.dataGroup, id);
         return newDiv;
     }
-    function makeBtn(className){
+
+    function makeBtn(className, id) {
         let newBtn = document.createElement('button');
         newBtn.setAttribute('class', className);
+        newBtn.setAttribute(DOMStrings.dataGroup, id);
         return newBtn;
     }
     return {
@@ -65,31 +78,42 @@ let UIController = (function () {
         getPopUpCloser: function () {
             return closeBtn;
         },
+        getBigListCloseButtons: function () {
+            return rmvListBtn;
+        },
         getDOMStrings: function () {
             return DOMStrings;
         },
+        findBigListX,
+        removeBigList:function (id) {
+            let bigToRemove = document.querySelectorAll("."+DOMStrings.cardHolder);
+            bigToRemove.forEach((card) =>{
+                if(card.getAttribute(DOMStrings.dataGroup) === id){
+                    card.remove();
+                }
+            });
+        },
         renderEmptyList: function (name, id) {
             console.log(name + " id" + id);
-            let cardHolder = makeDiv(DOMStrings.cardHolder);
-            cardHolder.setAttribute("data-group",id);
+            let cardHolder = makeDiv(DOMStrings.cardHolder, id);
 
 
             //create card's header
-            let cardHeader = makeDiv(DOMStrings.cardHead);
-            let cardHeadTitle = makeDiv(DOMStrings.cardHeadTitle);
+            let cardHeader = makeDiv(DOMStrings.cardHead, id);
+            let cardHeadTitle = makeDiv(DOMStrings.cardHeadTitle, id);
             cardHeadTitle.textContent = name;
-            let cardHeadClose = makeBtn(DOMStrings.cardHeadClose);
+            let cardHeadClose = makeBtn(DOMStrings.cardHeadClose, id);
             cardHeadClose.textContent = "X";
             cardHeader.appendChild(cardHeadTitle);
             cardHeader.appendChild(cardHeadClose);
 
             //create card's body
-            let cardBody = makeDiv(DOMStrings.cardBody);
+            let cardBody = makeDiv(DOMStrings.cardBody, id);
 
 
             //create card's footer
-            let cardFooter = makeDiv(DOMStrings.cardFooter);
-            let cardFooterAddBtn = makeBtn(DOMStrings.cardFooterAddBtn);
+            let cardFooter = makeDiv(DOMStrings.cardFooter, id);
+            let cardFooterAddBtn = makeBtn(DOMStrings.cardFooterAddBtn, id);
             cardFooterAddBtn.textContent = "+ Add Item";
             cardFooter.appendChild(cardFooterAddBtn);
 
@@ -120,27 +144,52 @@ let UIController = (function () {
 
 
 let MainController = (function (UICtrl, LogicCtrl) {
+    let DOMStrings = UICtrl.getDOMStrings();
     //add eventlistener for each list's add button
 
 
     //!NEW LIST MAKER
-    //popup menu when pressed
+    //popup the menu when pressed
     let newToDoBtn = UICtrl.getNewTodoButton();
     newToDoBtn.addEventListener('click', UICtrl.newListToggle);
+
     //do something if they press submit
     let userIn = document.querySelector("#userInput");
     userIn.onsubmit = function (e) {
         e.preventDefault();
         //addBookToLibrary(new Book(userIn.title.value, userIn.author.value, userIn.pages.value, userIn.read.checked));
-        
+
         //in logic side add a new empty list to the bigList array
         let id = LogicCtrl.addEmptyTodo(userIn.title.value);
+        //in the UI side update visual
         UICtrl.renderEmptyList(userIn.title.value, id);
         UICtrl.newListToggle();
+        UICtrl.findBigListX();
+        bigListCloseEvent();
     }
+
     //just close if they press x
     let pressX = UICtrl.getPopUpCloser();
     pressX.addEventListener('click', UICtrl.newListToggle);
+
+    //!REMOVE BIG LIST ON X PRESS
+    bigListCloseEvent();
+
+
+
+
+    //update biglist close button action
+    function bigListCloseEvent() {
+        let bigListRemove = UICtrl.getBigListCloseButtons();
+        bigListRemove.forEach((button) => {
+            button.addEventListener('click', () => {
+                let id=button.getAttribute(DOMStrings.dataGroup);
+                //TODO IMPLEMENT THESE FUNCTIONS
+                //LogicCtrl.removeBigList(id);
+                UICtrl.removeBigList(id);
+            });
+        });
+    }
 
 
     function init() {
